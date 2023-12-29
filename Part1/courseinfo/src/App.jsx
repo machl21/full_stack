edb178b0db12a9ae85ce9dd132e5ab68f91511be
machl21 <mangelcl21@gmail.com>
2023-12-29 14:43:28 -0500

    correcion

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
+  const part2 = 'Using props to pass dataa'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
